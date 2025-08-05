991dd0ea5e33873285d07949ae2eb72ee8462506
Armin Siegrist <armin.siegrist@hest.ethz.ch>
2025-08-05 15:25:52 +0200

    created InstallCommand file

--- ppplca/Commands/InstallCommand.py
+++ ppplca/Commands/InstallCommand.py
@@ -1,13 +1,28 @@
 class InstallCommand:
 
+    def __init__(self):
+        pass
+    
+    def handle(self):
+        self.publish_files("config.ini")
+        self.publish_files("Processing_data.xlsx")
+        self.publish_files("value_chains_test.xlsx")
+        self.publish_database_folder()
+    
     @staticmethod
-    def handle():
-        self.publish_config()
-        self.publish_xlsx()
-    
-    
-    def publish_config():
-        print("Publishing configuration files...")
+    def publish_files(filename):
+        import shutil
+        import os
+        import importlib.resources as resources
 
-    def publish_xlsx():
-        print("Publishing xlsx files...")
+        print(f"Publishing {filename} files...")
+
+        package = "ppplca.stubs"  # adjust if your folder is nested differently
+        with resources.path(package, filename) as src_path:
+            dest_path = os.path.join(os.getcwd(), filename)
+            shutil.copyfile(src_path, dest_path)
+            print(f"Copied {filename} to {dest_path}")
+
+    @staticmethod
+    def publish_database_folder():
+        print("Publishing database folder...")
