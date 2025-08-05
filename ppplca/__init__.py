8dbf4e355cf16270e4e359a11dcc3dfa2623cbd1
Armin Siegrist <armin.siegrist@hest.ethz.ch>
2025-08-05 15:39:01 +0200

    debugging

--- ppplca/__init__.py
+++ ppplca/__init__.py
@@ -1,15 +1,15 @@
 
 def install():
-    from Commands.InstallCommand import InstallCommand
+    from .Commands.InstallCommand import InstallCommand
     installcommand = InstallCommand()
     installcommand.handle()
 
 def setup():
-    from Commands.SetupDatabaseCommand import SetupDatabaseCommand
+    from .Commands.SetupDatabaseCommand import SetupDatabaseCommand
     setupdatabasecommand = SetupDatabaseCommand()
     setupdatabasecommand.handle()
 
 def run():
-    from Commands.RunCommand import RunCommand
+    from .Commands.RunCommand import RunCommand
     rundatabasecommand = RunCommand()
     rundatabasecommand.handle()
