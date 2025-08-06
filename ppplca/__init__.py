533167f1a58c2d995232172e1cff4003fbb00481
Armin Siegrist <armin.siegrist@hest.ethz.ch>
2025-08-06 08:56:39 +0200

    minor modifications

--- ppplca/__init__.py
+++ ppplca/__init__.py
@@ -5,15 +5,19 @@ def install():
     installcommand.handle()
 
 def setup():
-    answer = input("Have you updated the config.ini, Processing_data.xlsx, and value_chains_test.xlsx files? [Y/n]: ")
+    answer = input("Have you updated the config.ini file and saved the agrifootprint database? [Y/n]: ")
     if answer.lower() != "y":
-        print("Please update the files before running the setup.")
+        print("Please update the file and save the database before running the setup.")
         return
     from ppplca.Commands.SetupDatabaseCommand import SetupDatabaseCommand
     setupdatabasecommand = SetupDatabaseCommand()
     setupdatabasecommand.handle()
 
 def run():
+    answer = input("Have you updated the Processing_data.xlsx and value_chains_test.xlsx files? [Y/n]: ")
+    if answer.lower() != "y":
+        print("Please update the filese before running the setup.")
+        return
     from ppplca.Commands.RunCommand import RunCommand
     rundatabasecommand = RunCommand()
     rundatabasecommand.handle()
